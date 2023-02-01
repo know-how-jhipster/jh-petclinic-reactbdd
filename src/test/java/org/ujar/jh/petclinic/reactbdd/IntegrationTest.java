a0485dd641778ddaf3fc2afd99b9fbe6a9cdfc01
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-02-01 15:33:49 +0200

    Initial version of petclinic generated by generator-jhipster@7.9.3
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/test/java/org/ujar/jh/petclinic/reactbdd/IntegrationTest.java
@@ -0,0 +1,28 @@
+package org.ujar.jh.petclinic.reactbdd;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.ujar.jh.petclinic.reactbdd.PetclinicApp;
+import org.ujar.jh.petclinic.reactbdd.config.AsyncSyncConfiguration;
+import org.ujar.jh.petclinic.reactbdd.config.EmbeddedElasticsearch;
+import org.ujar.jh.petclinic.reactbdd.config.EmbeddedSQL;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { PetclinicApp.class, AsyncSyncConfiguration.class })
+@EmbeddedElasticsearch
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+    // 5s is the spring default https://github.com/spring-projects/spring-framework/blob/29185a3d28fa5e9c1b4821ffe519ef6f56b51962/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
---
+++ src/test/java/org/ujar/jh/petclinic/reactbdd/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package org.ujar.jh.petclinic.reactbdd.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.ujar.jh.petclinic.reactbdd.IntegrationTest;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
