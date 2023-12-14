b2e695ed1536fc2e7ab429bdaf557cbd6bf37192
Dima Denysenko <coding@dimdnk.com>
2023-12-14 16:26:29 +0200

    chore: migrate to dev.knowhowto group

--- src/test/java/org/ujar/jh/petclinic/reactbdd/cucumber/CucumberIT.java
+++
@@ -1,8 +0,0 @@
-package org.ujar.jh.petclinic.reactbdd.cucumber;
-
-import io.cucumber.junit.platform.engine.Cucumber;
-import org.ujar.jh.petclinic.reactbdd.IntegrationTest;
-
-@Cucumber
-@IntegrationTest
-class CucumberIT {}
